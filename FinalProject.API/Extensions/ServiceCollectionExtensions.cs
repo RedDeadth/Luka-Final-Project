1fe5906e0729e0052075a72eb741f0a81123a1e6
redDeadth <mimamamemima335@gmail.com>
2025-11-08 00:24:49 -0500

    IMplemente de moejor manera la arquitectura hexagonal

--- FinalProject.API/Extensions/ServiceCollectionExtensions.cs
+++ FinalProject.API/Extensions/ServiceCollectionExtensions.cs
@@ -1,18 +1,14 @@
-namespace FinalProject.Presentation.Extensions;
+namespace FinalProject.API.Extensions;
 
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        // Registrar servicios de aplicación aquí
-        // Ejemplo: services.AddScoped<IMyService, MyService>();
         return services;
     }
 
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
     {
-        // Registrar repositorios y servicios de infraestructura aquí
-        // Ejemplo: services.AddScoped<IMyRepository, MyRepository>();
         return services;
     }
 }
---
+++ FinalProject.API/Program.cs
@@ -0,0 +1,21 @@
+using FinalProject.API.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
